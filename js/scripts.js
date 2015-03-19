34a6fb350cdef366e935240713e8261c27c72375
LLstarla <loraleestarla@me.com>
2015-03-19 15:24:53 -0700

    v5

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,10 @@ $(document).ready(function() {
       $('#fullpage').fullpage({
         verticalCentered: false,
         continuousVertical: true,
+        slidesNavigation: true,
+        responsive: 700,
+        scrollingSpeed: 1000,
+        scrollOverflow: true,
         css3:false
       });
     });
