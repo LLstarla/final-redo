8332324a2128b30a3b1198fa7999414059bf167e
LLstarla <loraleestarla@me.com>
2015-03-19 13:09:13 -0700

    v2

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
-// Start midnight
-$(document).ready(function(){
-  // Change this to the correct selector.
-  $('nav.fixed').midnight();
-});
+$(document).ready(function() {
+      $('#fullpage').fullpage({
+        verticalCentered: false,
+
+        //to avoid problems with css3 transforms and fixed elements in Chrome, as detailed here: https://github.com/alvarotrigo/fullPage.js/issues/208
+        css3:false
+      });
+    });
