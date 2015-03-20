9769f7ddc7546f829a5e9bc3a211567673ab8889
LLstarla <loraleestarla@me.com>
2015-03-20 05:31:49 -0700

    v15

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
       $('#fullpage').fullpage({
         verticalCentered: false,
         continuousVertical: true,
-        slidesNavigation: true,
         responsive: 700,
         scrollingSpeed: 1000,
         css3:false
